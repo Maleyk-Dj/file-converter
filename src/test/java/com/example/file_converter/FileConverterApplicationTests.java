63d4653920e5104ef67fd66fdc734b704c18f556
Maleyk <m9375774872@gmail.com>
2026-06-26 23:13:29 +0300

    "Базовая структура проекта"

---
+++ src/test/java/com/example/file_converter/FileConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
