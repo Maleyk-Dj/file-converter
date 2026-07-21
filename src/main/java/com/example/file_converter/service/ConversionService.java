23d503085e476f73706b71e3d5261228a866c1ad
Maleyk <m9375774872@gmail.com>
2026-07-22 00:39:37 +0300

    refactor: вынесена логика MinIO в MinioService, добавлены presigned url

--- src/main/java/com/example/file_converter/service/ConversionService.java
+++ src/main/java/com/example/file_converter/service/ConversionService.java
@@ -5,23 +5,18 @@ import com.example.file_converter.converter.FileConverter;
 import com.example.file_converter.exception.FileConversionException;
 import com.example.file_converter.model.FileConversionRequest;
 import com.example.file_converter.model.FileConversionResult;
-import io.minio.GetObjectArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ConversionService {
 
-    private final MinioClient minioClient;
     private final List<FileConverter> converters;
     private final MinioProperties minioProperties;
+    private final MinioService minioService;
 
     public FileConversionResult convert(FileConversionRequest request) {
         try {
@@ -30,15 +25,7 @@ public class ConversionService {
                 throw new FileConversionException("У файла нет расширения: " + request.getFilePath());
             }
 
-            byte[] fileBytes;
-            try (InputStream inputStream = minioClient.getObject(
-                    GetObjectArgs.builder()
-                            .bucket(request.getBucket())
-                            .object(request.getFilePath())
-                            .build()
-            )) {
-                fileBytes = inputStream.readAllBytes();
-            }
+            byte[] fileBytes = minioService.download(request.getBucket(), request.getFilePath());
 
             String extension = request.getFilePath().substring(index + 1);
 
@@ -51,14 +38,8 @@ public class ConversionService {
 
             String pdfFileName = request.getFilePath().substring(0, index) + ".pdf";
 
-            minioClient.putObject(
-                    PutObjectArgs.builder()
-                            .bucket(minioProperties.getConvertedBucket())
-                            .object(pdfFileName)
-                            .stream(new ByteArrayInputStream(pdfBytes), pdfBytes.length, -1)
-                            .contentType("application/pdf")
-                            .build()
-            );
+            minioService.upload(minioProperties.getConvertedBucket(), pdfFileName,
+                    pdfBytes, "application/pdf");
 
             return FileConversionResult.success(
                     request.getMessageId(),
